c853e386b982acd4112734162f24acf81e7692b1
Bruce Perry <bruce.perry@momentum-tech.ca>
2024-07-12 14:56:01 -0400

    updated packages + refactor

--- src/ToastProvider.tsx
+++ src/ToastProvider.tsx
@@ -1,53 +1,57 @@
-import React, {
-    FC, useCallback,  useState,
-} from 'react';
-import { EvaStatus } from '@ui-kitten/components/devsupport';
-import { Toast, ToastProps } from './Toast';
+import React, { FC, useCallback, useState } from "react";
+import { EvaStatus } from "@ui-kitten/components/devsupport";
+import { Toast, ToastProps } from "./Toast";
 
 interface ShowToastOptions {
-    message: string | React.ReactNode
-    status: EvaStatus
-    duration?: number
+  message: string | React.ReactNode;
+  status: EvaStatus;
+  duration?: number;
 }
 
 interface ToastCtx {
-    showToast: (options: ShowToastOptions) => void
+  showToast: (options: ShowToastOptions) => void;
 }
 
 export const ToastContext = React.createContext<ToastCtx>({
-    showToast: () => null,
+  showToast: () => null,
 });
 
 export const useToast = () => React.useContext(ToastContext);
 
-export const ToastProvider: FC = ({ children }) => {
-    const [toasts, setToasts] = useState<ToastProps[]>([]);
+export const ToastProvider = ({ children }: { children: React.ReactNode }) => {
+  const [toasts, setToasts] = useState<ToastProps[]>([]);
 
-    const onClose = useCallback((id: string) => {
-        setToasts(prevToasts => prevToasts.filter((toast) => toast.id !== id));
-    }, [setToasts]);
+  const onClose = useCallback(
+    (id: string) => {
+      setToasts((prevToasts) => prevToasts.filter((toast) => toast.id !== id));
+    },
+    [setToasts]
+  );
 
-    const showToast = useCallback((options: ShowToastOptions) => {
-        setToasts(prevToasts => [...prevToasts, {
-            id: `${Math.random()}`,
-            status: options.status,
-            message: options.message,
-            duration: options.duration || 5000,
-            onClose,
-        }]);
-    }, [setToasts]);
+  const showToast = useCallback(
+    (options: ShowToastOptions) => {
+      setToasts((prevToasts) => [
+        ...prevToasts,
+        {
+          id: `${Math.random()}`,
+          status: options.status,
+          message: options.message,
+          duration: options.duration || 5000,
+          onClose,
+        },
+      ]);
+    },
+    [setToasts]
+  );
 
-    return (
-        <ToastContext.Provider value={{ showToast }}>
-            {children}
-            <>
-                {toasts.map(toast => (
-                    <Toast
-                        {...toast}
-                        key={toast.id}
-                    />
-                ))}
-            </>
-        </ToastContext.Provider>
-    );
+  return (
+    <ToastContext.Provider value={{ showToast }}>
+      {children}
+      <>
+        {toasts.map((toast) => (
+          <Toast {...toast} key={toast.id} />
+        ))}
+      </>
+    </ToastContext.Provider>
+  );
 };
